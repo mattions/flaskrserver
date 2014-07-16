f320ee5affe7e1fc0db6cdb46eea962479956570
Michele Mattioni <mattions@gmail.com>
2014-07-17 00:36:30 +0100

    Autoremove old plots.

--- server.py
+++ server.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template, url_for
 import subprocess
 import uuid
 import os
+import time
+
 app = Flask(__name__)
+
+SECONDS_IN_A_DAY = 86400
 
 if not os.path.exists("static/plots"):
     os.mkdir("static/plots")
 
+def cleanup_old_plot(path):
+    now = time.time()
+    for _, _, files in os.walk(path):
+        for filename in files:
+            file_path = os.path.join(path, filename)
+            mtime_file = os.stat(file_path).st_mtime
+            print mtime_file
+            if now - mtime_file > SECONDS_IN_A_DAY :
+                os.remove(file_path)
+        
+
 @app.route("/rscript")
 def rscript():
+    cleanup_old_plot("static/plots")
     meme_path = url_for('static', filename='displayalltherplots.jpg')
     image_filename = "{0}_{1}".format("rplot", uuid.uuid4().hex)
     image_path = os.path.join("plots", image_filename)
